aa1b3c4f493447146bf2312526045b91aacf5fb7
xonx4l <sk797119@gmail.com>
2024-02-25 17:12:00 +0530

    bdk query

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
+use anyhow::Result;
 use bdk::{bitcoin::Network, database::MemoryDatabase, Wallet};
 use std::env;
 
-fn function_that_takes_strins(s: String) {
+fn function_that_takes_strings(s: String) {
     println!("String: {}", s);
 }
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     dotenv::from_filename(".env").ok();
     dotenv::dotenv().ok();
 
@@ -16,15 +17,21 @@ fn main() {
         Err(e) => panic!("Error: {}", e),
     };
 
-    println!("Descriptor: {}", descriptor);
+    //println!("Descriptor: {}", descriptor);
 
-    let str_desc = "WALLET_DESCRIPTOR";
-    let string_desc = String::from(str_desc);
+    //let str_desc = "WALLET_DESCRIPTOR";
+    //let string_desc = String::from(str_desc);
+
+    // function_that_takes_strings(&descriptor);
 
     let wallet = Wallet::new(
-        &descriptor.clone(),
+        &descriptor,
         None,
         Network::Testnet,
         MemoryDatabase::default(),
     )?;
+
+    dbg!(wallet);
+
+    Ok(())
 }
